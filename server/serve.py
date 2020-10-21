851e16a3b52b01408619efa78f8cdfd9f9658546
Parth Raghav <iniento12@gmail.com>
2020-10-21 23:31:02 +0530

    Added API to serve static files

--- server/serve.py
+++ server/serve.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 
-@app.route("/test")
-def hello():
-    return "Hello, World!"
+@app.route("/get_accessible_image/<path:path>")
+def hello(path):
+    return send_from_directory('../tests', path)
 
 
 if __name__ == "__main__":
