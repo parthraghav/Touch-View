4869e361a7c427201e18f3fe8093056f4b291351
Parth Raghav <iniento12@gmail.com>
2020-12-02 06:34:45 +0530

    integrated controller with widget

--- renderer/lib/touch_view.dart
+++ renderer/lib/touch_view.dart
@@ -2,50 +2,59 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:renderer/controller.dart';
 import 'package:renderer/matrix.dart';
 import 'package:renderer/measure_size_render_object.dart';
 
 class TouchView extends StatefulWidget {
-  Image imageProvider;
+  String imageName;
+  bool reveal;
 
-  TouchView({Key key, this.imageProvider}) : super(key: key);
+  TouchView({Key key, this.imageName, this.reveal}) : super(key: key);
 
   @override
   _TouchViewState createState() => _TouchViewState();
 }
 
-class _TouchViewState extends State<TouchView> {
+class _TouchViewState extends State<TouchView> with TickerProviderStateMixin {
+  TouchViewController _controller;
   Offset _offset = Offset.zero;
-  double _scale = 1.0;
-  double _minScale = 1.0;
+  double _minScale = 0.9;
   double _maxScale = 3.0;
+  double _scale = 0.9;
   Size _indicatorSize = Size(100, 100);
   Size _containerSize = Size.zero;
   Size _imageSize = Size.zero;
+  bool _shouldReveal = true;
+
+  Animation _scaleAnimation;
+  AnimationController _scaleAnimationController;
 
   @override
   void initState() {
     super.initState();
+    _controller = TouchViewController(widget.imageName);
+
+    _scaleAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+    _scaleAnimation = Tween(begin: _minScale, end: _maxScale).animate(
+        CurvedAnimation(
+            curve: Curves.easeInCubic, parent: _scaleAnimationController));
+    setState(() {
+      _shouldReveal = widget.reveal;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     //Matrix4.translationValues(offset.dx, offset.dy, 0.0)
     return Container(
-      child: MeasureSize(
-        onChange: (size) {
-          print(size);
-          setState(() {
-            _containerSize = size;
-          });
-        },
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: <Widget>[
-            _buildImageTransformer(),
-            _buildTargetIndicator(),
-          ],
-        ),
+      child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: <Widget>[
+          _buildImageTransformer(),
+          _buildTargetIndicator(),
+        ],
       ),
     );
   }
@@ -59,75 +68,45 @@ class _TouchViewState extends State<TouchView> {
             _imageSize = size;
           });
         },
-        child: Transform(
-          transform: Matrix4.identity()
-            ..translate(_offset.dx, _offset.dy)
-            ..scale(_scale)
-            ..rotateZ(0),
-          alignment: AlignmentDirectional.center,
-          child: GestureDetector(
-            onPanUpdate: (details) => setState(() {
-              // Configure a bounding box for the indicator
-              final indicator = Rectangle(
-                _containerSize.width / 2 - _indicatorSize.width / 2, // left
-                _containerSize.height / 2 - _indicatorSize.height / 2, // top
-                _indicatorSize.width, // width
-                _indicatorSize.height, // height
-              );
+        child: AnimatedBuilder(
+          animation: _scaleAnimationController,
+          builder: (context, child) {
+            return Transform(
+              transform: Matrix4.identity()
+                ..translate(_offset.dx, _offset.dy)
+                ..scale(_scaleAnimation.value)
+                ..rotateZ(0),
+              alignment: AlignmentDirectional.center,
+              child: GestureDetector(
+                onPanUpdate: (details) => setState(() {
+                  final i = (_scale * _imageSize.width / 2 - _offset.dx) *
+                      _controller.dimensions.width ~/
+                      (_scale * _imageSize.width);
+                  final j = (_scale * _imageSize.height / 2 - _offset.dy) *
+                      _controller.dimensions.height ~/
+                      (_scale * _imageSize.height);
+                  print("$i,$j ${_controller.getCurrentLabel(i, j)}");
 
-              // Figure out the delta from the pan
-              final delta = details.delta.scale(_scale, _scale);
-
-              // Configure the bounding box of the transformed image
-              final image = Rectangle(
-                  _offset.dx, // left
-                  _offset.dy, // top
-                  _imageSize.width, // width
-                  _imageSize.height // height
-                  );
-
-              // if (delta.dy >= 0) {
-              //   if (image.bottom < indicator.bottom) {
-              //     _offset += Offset(0, delta.dy);
-              //     print("Aight");
-              //   } else {
-              //     print("Oops");
-              //   }
-              // } else if (delta.dy < 0) {
-              //   if (image.top > indicator.top) {
-              //     _offset += Offset(0, delta.dy);
-              //     print("Aight");
-              //   } else {
-              //     print("Oops");
-              //   }
-              // }
-
-              // if (delta.dx >= 0) {
-              //   if (image.left < indicator.left) {
-              //     _offset += Offset(delta.dx, 0);
-              //     print("Aight");
-              //   } else {
-              //     print("Oops");
-              //   }
-              // } else if (delta.dx < 0) {
-              //   if (image.right > indicator.right) {
-              //     _offset += Offset(delta.dx, 0);
-              //     print("Aight");
-              //   } else {
-              //     print("Oops");
-              //   }
-              // }
-
-              // print("dy ${delta.dy.isNegative}");
-              // print("dx ${delta.dx.isNegative}");
-              _offset += details.delta.scale(_scale, _scale);
-            }),
-            onDoubleTap: () => setState(() {
-              _offset = Offset.zero;
-              _scale = (_scale == 1.0) ? _maxScale : _minScale;
-            }),
-            child: widget.imageProvider,
-          ),
+                  _offset += details.delta.scale(_scale, _scale);
+                }),
+                onDoubleTap: () => _didDoubleTap(),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.all(const Radius.circular(10)),
+                  ),
+                  child: AnimatedOpacity(
+                    duration: Duration(milliseconds: 200),
+                    opacity: widget.reveal ? 1.0 : 0.01,
+                    child: ClipRRect(
+                        borderRadius:
+                            BorderRadius.all(const Radius.circular(10)),
+                        child: Image.network(_controller.imagePath)),
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -155,5 +134,18 @@ class _TouchViewState extends State<TouchView> {
         ),
       ),
     );
+  }
+
+  _didDoubleTap() {
+    setState(() {
+      _offset = Offset.zero;
+    });
+    if (_scale == _minScale) {
+      _scale = _maxScale;
+      _scaleAnimationController.forward();
+    } else {
+      _scale = _minScale;
+      _scaleAnimationController.reverse();
+    }
   }
 }
