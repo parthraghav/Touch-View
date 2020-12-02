206409189893d503858249ca9e0dfb70e922003a
Parth Raghav <iniento12@gmail.com>
2020-12-02 06:34:14 +0530

    update public methods in matrix class

--- renderer/lib/matrix.dart
+++ renderer/lib/matrix.dart
@@ -1,10 +1,15 @@
 class Matrix {
   // The inner datatype used to store matrix values
   List<List<int>> _mat;
+  // The inner datatype used to store matrix values in a folded form
+  List<int> _vec;
   // Number of columns in the matrix (or Matrix's width)
   final int _w;
   // Number of rows in the matrix (or Matrix's height)
   final int _h;
+
+  int get w => _w;
+  int get h => _h;
 
   // Instance constructor to initialise the dimensions
   // of the matrix
@@ -15,6 +20,8 @@ class Matrix {
   void read(List<int> vec) {
     // Check if dimensions match
     assert(_w * _h == vec.length);
+    // Store value of vec
+    _vec = vec;
     // Fill the values
     _mat = List.generate(
         _h, (j) => List.generate(_w, (i) => vec[j * _w + i], growable: false),
@@ -24,4 +31,32 @@ class Matrix {
   // Overload list access operator. It returns a row of type [List<int>]
   // which already provides a setter and getter API.
   operator [](int j) => _mat[j];
+
+  // Method to import matrix from a comma seperated string
+  factory Matrix.fromCSV(String text, Dimensions dim) {
+    final mat = Matrix(
+      dim.width.round(),
+      dim.height.round(),
+    );
+    final bigVec = text.split(',').map(int.parse).toList();
+    mat.read(bigVec);
+    return mat;
+  }
+
+  // Method to export matrix into CSV format
+  String toCSV() {
+    if (_mat == null) throw new Exception("Matrix is not yet initialised");
+    return _vec.join(",");
+  }
+
+  // Check if an index exists within the matrix
+  bool includes(int x, int y) {
+    return (x >= 0 && x < _w) && (y >= 0 && y < _h);
+  }
+}
+
+class Dimensions {
+  int width;
+  int height;
+  Dimensions(this.width, this.height);
 }
