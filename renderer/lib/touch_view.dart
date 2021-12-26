1a9e6a13ec96fcea0904fe646cc453dedf0bcabd
Parth Raghav <iniento12@gmail.com>
2021-12-26 16:15:25 +0530

    pushed changes

--- renderer/lib/touch_view.dart
+++ renderer/lib/touch_view.dart
@@ -23,12 +23,12 @@ class _TouchViewState extends State<TouchView> with TickerProviderStateMixin {
   double _maxScale = 3.0;
   double _scale = 0.9;
   Size _indicatorSize = Size(100, 100);
-  Size _containerSize = Size.zero;
   Size _imageSize = Size.zero;
-  bool _shouldReveal = true;
 
   Animation _scaleAnimation;
+  Animation _panAnimation;
   AnimationController _scaleAnimationController;
+  AnimationController _panAnimationController;
 
   @override
   void initState() {
@@ -40,9 +40,15 @@ class _TouchViewState extends State<TouchView> with TickerProviderStateMixin {
     _scaleAnimation = Tween(begin: _minScale, end: _maxScale).animate(
         CurvedAnimation(
             curve: Curves.easeInCubic, parent: _scaleAnimationController));
-    setState(() {
-      _shouldReveal = widget.reveal;
-    });
+
+    _doPan(toOffset: Offset(0, 0));
+  }
+
+  @override
+  void dispose() {
+    _scaleAnimationController.dispose();
+    _panAnimationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -63,7 +69,6 @@ class _TouchViewState extends State<TouchView> with TickerProviderStateMixin {
     return Center(
       child: MeasureSize(
         onChange: (size) {
-          print(size);
           setState(() {
             _imageSize = size;
           });
@@ -71,41 +76,53 @@ class _TouchViewState extends State<TouchView> with TickerProviderStateMixin {
         child: AnimatedBuilder(
           animation: _scaleAnimationController,
           builder: (context, child) {
-            return Transform(
-              transform: Matrix4.identity()
-                ..translate(_offset.dx, _offset.dy)
-                ..scale(_scaleAnimation.value)
-                ..rotateZ(0),
-              alignment: AlignmentDirectional.center,
-              child: GestureDetector(
-                onPanUpdate: (details) => setState(() {
-                  final i = (_scale * _imageSize.width / 2 - _offset.dx) *
-                      _controller.dimensions.width ~/
-                      (_scale * _imageSize.width);
-                  final j = (_scale * _imageSize.height / 2 - _offset.dy) *
-                      _controller.dimensions.height ~/
-                      (_scale * _imageSize.height);
-                  print("$i,$j ${_controller.getCurrentLabel(i, j)}");
+            return AnimatedBuilder(
+                animation: _panAnimationController,
+                builder: (context, child) {
+                  final dx = _panAnimation.value.dx;
+                  final dy = _panAnimation.value.dy;
 
-                  _offset += details.delta.scale(_scale, _scale);
-                }),
-                onDoubleTap: () => _didDoubleTap(),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white, width: 2),
-                    borderRadius: BorderRadius.all(const Radius.circular(10)),
-                  ),
-                  child: AnimatedOpacity(
-                    duration: Duration(milliseconds: 200),
-                    opacity: widget.reveal ? 1.0 : 0.01,
-                    child: ClipRRect(
-                        borderRadius:
-                            BorderRadius.all(const Radius.circular(10)),
-                        child: Image.network(_controller.imagePath)),
-                  ),
-                ),
-              ),
-            );
+                  return GestureDetector(
+                    onDoubleTap: () => _didDoubleTap(),
+                    child: Listener(
+                      onPointerMove: (PointerMoveEvent evt) {
+                        // print(evt.delta);
+                        final i = (_scale * _imageSize.width / 2 - _offset.dx) *
+                            _controller.dimensions.width ~/
+                            (_scale * _imageSize.width);
+                        final j =
+                            (_scale * _imageSize.height / 2 - _offset.dy) *
+                                _controller.dimensions.height ~/
+                                (_scale * _imageSize.height);
+                        print("$i,$j ${_controller.getCurrentLabel(i, j)}");
+
+                        _doPan(withOffset: evt.delta);
+                      },
+                      child: Transform(
+                        transform: Matrix4.identity()
+                          ..translate(dx, dy)
+                          ..scale(_scaleAnimation.value)
+                          ..rotateZ(0),
+                        alignment: AlignmentDirectional.center,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white, width: 2),
+                            borderRadius:
+                                BorderRadius.all(const Radius.circular(10)),
+                          ),
+                          child: AnimatedOpacity(
+                            duration: Duration(milliseconds: 200),
+                            opacity: widget.reveal ? 1.0 : 0.01,
+                            child: ClipRRect(
+                                borderRadius:
+                                    BorderRadius.all(const Radius.circular(10)),
+                                child: Image.network(_controller.imagePath)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                });
           },
         ),
       ),
@@ -137,15 +154,37 @@ class _TouchViewState extends State<TouchView> with TickerProviderStateMixin {
   }
 
   _didDoubleTap() {
-    setState(() {
-      _offset = Offset.zero;
-    });
+    _doPan(toOffset: Offset.zero);
     if (_scale == _minScale) {
       _scale = _maxScale;
       _scaleAnimationController.forward();
     } else {
       _scale = _minScale;
       _scaleAnimationController.reverse();
+    }
+  }
+
+  bool _isPanAnimationComplete = true;
+  _doPan({Offset toOffset, Offset withOffset}) {
+    assert(toOffset == null || withOffset == null);
+    final finalOffset = (toOffset != null) ? toOffset : _offset + withOffset;
+    if (_isPanAnimationComplete) {
+      _panAnimationController = AnimationController(
+          vsync: this, duration: Duration(milliseconds: 50));
+      _panAnimation = Tween(begin: _offset, end: finalOffset).animate(
+          CurvedAnimation(
+              curve: Curves.linear, parent: _panAnimationController))
+        ..addStatusListener((status) {
+          if (status == AnimationStatus.completed) {
+            // print("i waass called");
+            setState(() {
+              _offset = _panAnimation.value;
+              _isPanAnimationComplete = true;
+            });
+          }
+        });
+      _panAnimationController.forward();
+      setState(() => _isPanAnimationComplete = false);
     }
   }
 }
